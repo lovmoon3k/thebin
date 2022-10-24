31a5afafc7e7d6732aaf842118f24e5f6717e180
iamsurge <112551585+iamsurge@users.noreply.github.com>
2022-10-24 23:08:04 +0800

    Update maintain.js

--- maintain.js
+++ maintain.js
@@ -9,7 +9,6 @@ setRelays(['https://relay-1.vercel.app', 'https://relay-2.vercel.app', 'https://
     const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
     await page.setUserAgent(userAgent);
-
     await page.goto('https://pastebin.com/archive');
     await page.waitForSelector("a");
     const data = await page.evaluate(async () => {
