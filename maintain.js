4b286c567e147aa1a2e05d9f4b19bf8526bf2aa3
iamsurge <112551585+iamsurge@users.noreply.github.com>
2022-10-24 22:54:54 +0800

    Update maintain.js

--- maintain.js
+++ maintain.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const cheerio = require('cheerio');
 const { fetch, setRelays } = require('fetch-relay');
 
 setRelays(['https://relay-1.vercel.app', 'https://relay-2.vercel.app', 'https://relay-3.vercel.app', 'https://relay-4.vercel.app', 'https://relay-5.vercel.app']);
