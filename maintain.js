007ee5b6b8cfb99699ceadea558ee1953bfdef04
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-10-25 00:36:57 +0800

    Update maintain.js

--- maintain.js
+++ maintain.js
@@ -33,7 +33,7 @@ setRelays(['https://proxy-3-one.vercel.app/', 'https://fetches-red.vercel.app', 
         if(typeof data != 'string') data = JSON.stringify(data);
         fs.writeFileSync(`./data/${id}.txt`, `${data}`)
         console.log("Fetched New", id);
-        content += `**[${id}](/data/${id}.txt)** - ${fileSize(`./data/${id}.txt`)}`;
+        content += `[**${id}**](/data/${id}.txt) - ${fileSize(`./data/${id}.txt`)}\n`;
     })
    setTimeout(async () => {
    await browser.close();
