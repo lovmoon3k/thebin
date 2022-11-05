71fbbd840cef243ba5bf0f5f0923de9ddccbbd84
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-05 13:35:01 +0800

    Update wrap.js

--- wrap.js
+++ wrap.js
@@ -2,14 +2,17 @@ const fs = require('fs');
 const fLine = require('firstline');
 const total = fs.readdirSync('./data').filter(file => file.endsWith('.txt'));
 if(!fs.existsSync(`./data/indexFile.text`)) fs.writeFileSync("./data/indexFile.text", "[]//index-files");
+var index = fs.readFileSync('./data/indexFile.text', 'utf-8');
+
+(async () => {
+var exist = await fLine('./data/indexFile.text', "//index-files");
+const array = JSON.parse(exist.slice(0, -13));
 total.forEach(async (e) => {
-  var index = fs.readFileSync('./data/indexFile.text', 'utf-8');
-  var exist = await fLine('./data/indexFile.text', "//index-files");
-  const array = JSON.parse(exist.slice(0, -13));
   if(array.includes(e))return;
   const data = fs.readFileSync(`./data/${e}`, 'utf-8');
   index = `${index}\n\n${data}`;
   array.push(e);
-  index = index.replace(exist, `${JSON.stringify(array)}//index-files`);
-  fs.writeFileSync("./data/indexFile.text", index);
 })
+index = index.replace(exist, `${JSON.stringify(array)}//index-files`);
+fs.writeFileSync("./data/indexFile.text", index);
+})();
