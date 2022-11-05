c5478aa673b6172c51ca73f316c525780c841bd4
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-05 13:27:14 +0800

    Update wrap.js

--- wrap.js
+++ wrap.js
@@ -5,12 +5,11 @@ if(!fs.existsSync(`./data/indexFile.text`)) fs.writeFileSync("./data/indexFile.t
 total.forEach(async (e) => {
   var index = fs.readFileSync('./data/indexFile.text', 'utf-8');
   var exist = await fLine('./data/indexFile.text', "//index-files");
-  console.log(exist, typeof exist);
   const array = JSON.parse(exist.slice(0, -13));
   if(array.includes(e))return;
   const data = fs.readFileSync(`./data/${e}`, 'utf-8');
-  array.push(e);
   index = `${index}\n\n${data}`;
   index = index.replace(exist, `${JSON.stringify(array)}//index-files`);
   fs.writeFileSync("./data/indexFile.text", index);
+  array.push(e);
 })
