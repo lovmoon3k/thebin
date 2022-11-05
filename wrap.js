715dd12b047caece17a7e07c569ffb56f358af53
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-11-05 13:17:25 +0800

    Create wrap.js

---
+++ wrap.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const fLine = require('firstline');
+const total = fs.readdirSync('./data').filter(file => file.endsWith('.txt'));
+if(!fs.existsSync(`./data/indexFile.text`)) fs.writeFileSync("./data/indexFile.text", "[]//index-files");
+total.forEach((e) => {
+  var index = fs.readFileSync('./data/indexFile.text', 'utf-8');
+  var exist = firstline('./data/indexFile.text', "//index-files");
+  const array = JSON.parse(exist.slice(0, -13));
+  if(array.includes(e))return;
+  const data = fs.readFileSync(`./data/${e}`, 'utf-8');
+  array.push(e);
+  index = `${index}\n\n${data}`;
+  index = index.split(exist).join(`${JSON.stringify(array)}//index-files`);
+  fs.writeFileSync("./data/indexFile.text", index);
+})
